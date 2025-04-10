8c917387fb63371ee99271dbc356f6f5d104f564
zakharova nastya <zaharova_nastya_2004@mail.ru>
2025-04-10 16:49:12 +0500

    init project

---
+++ Assets/Scripts/Movment.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Movment : MonoBehaviour
+{
+    public Sprite front;
+    public Sprite left;
+    public Sprite right;
+    public Sprite back;
+
+    public float moveSpeed;
+    private Rigidbody2D rigidbody2d;
+    private SpriteRenderer spriteRenderer;
+    private Vector2 movement;
+
+     void Start()
+    {
+        rigidbody2d = this.GetComponent<Rigidbody2D>();
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+    }
+
+    private void Update() {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate() {
+        if (movement.y > 0) {
+            spriteRenderer.sprite = back;
+        }
+        else if (movement.y < 0) {
+            spriteRenderer.sprite = front;
+        }
+        else if (movement.x > 0) {
+            spriteRenderer.sprite = right;
+        }
+        else if (movement.x < 0) {
+            spriteRenderer.sprite = left;
+        }
+        rigidbody2d.MovePosition(rigidbody2d.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
